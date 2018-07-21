fc795c21d154cc0c4625aa8da20a63ff7b756976
mohsen khodadadzadeh <mohsenkhodadadzadeh@mohsens-macbook-pro.local>
2018-07-21 10:11:50 +0430

    Initial Commit

---
+++ ObserverPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObserverPattern
+//
+//  Created by mohsen khodadadzadeh on 7/21/18.
+//  Copyright © 2018 mohsen khodadadzadeh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
